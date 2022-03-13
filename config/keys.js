71e711bde6824ece2c9dea90ae0cc0ed551bfb7f
Stow <91526733+naftiaziz@users.noreply.github.com>
2022-03-13 14:06:25 +0100

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //mongoURI: "mongodb://spectre:spectre007@ds253537.mlab.com:53537/elearning", //apurva's
+  mongoURI: 'mongodb+srv://aziz:aziz@cluster0.8ylgr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  secretOrKey: "secret"
+};
